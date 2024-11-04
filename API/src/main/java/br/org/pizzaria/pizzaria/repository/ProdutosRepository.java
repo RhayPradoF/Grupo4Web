9c200a202a6ec044119bd20eedcc20637f6c0e24
CaiqueROC <caique_ramos2010@hotmail.com>
2024-11-04 11:34:18 -0300

    inserindo metodo ProdutoInserirDTO

--- API/src/main/java/br/org/pizzaria/pizzaria/repository/ProdutosRepository.java
+++ API/src/main/java/br/org/pizzaria/pizzaria/repository/ProdutosRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.org.pizzaria.pizzaria.domain.Produtos;
+import br.org.pizzaria.pizzaria.dto.ProdutoInserirDTO;
 
 @Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, Long>{
+
+	Produtos save(ProdutoInserirDTO produtoInserirDTO);
 
 }
