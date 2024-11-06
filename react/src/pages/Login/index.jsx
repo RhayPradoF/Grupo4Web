4e104d53d16242ec87e886a3e694cec49cff8c35
WesseleyJr <wesseleyjr@gmail.com>
2024-11-06 09:51:48 -0300

    att

--- react/src/pages/Login/index.jsx
+++ react/src/pages/Login/index.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import Mail from '../../assets/EmailIcon.png';
-import Chave from '../../assets/Key.png'; 
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Mail from "../../assets/EmailIcon.png";
+import Chave from "../../assets/Key.png";
+import axios from "axios";
 
-import { 
+import {
   Container,
-  Title, 
+  Title,
   Subtitle,
-  InputContainer, 
-  Label, 
-  Input, 
-  ErrorMessage,
+  InputContainer,
+  Label,
+  Input,
+  //ErrorMessage,
   AreaTitle,
   AreaSubtitle,
   TotalTitleArea,
@@ -20,98 +21,77 @@ import {
   Button,
   MailIcone,
   ChaveIcone,
-   Form,
-} from './Login.style';
+  Form,
+} from "./Login.style";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
   const [senhaVisivel, setSenhaVisivel] = useState(false);
-  const [erroEntrada, setErroEntrada] = useState('');
-  const [user, setUser] = useState({});
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/RhayPradoF") //substituir pela nossa api
-    .then((resposta)=> resposta.json())
-    .then((json) => setUser(json))
-    .then(()=> setUser(resposta))
-    .catch("Problema na requisição");
-
-},[]);
-
-
-  const handleLogin = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (email === '' || senha === '') {
-      alert('Por favor, preencha todos os campos.');
-      return;
-    }
 
-    // Simular a chamada de API para verificar o login
-    setTimeout(() => {
-      if (email === 'admin@example.com' && senha === 'admin123') {
-        alert('Login realizado com sucesso!');
-        window.location.href = '/';
-      } else {
-        setErroEntrada('E-mail ou senha inválidos.');
-      }
-    }, 1000);
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/usuario/${email}`
+      );
+      console.log("Resposta da API:", response.data);
+    } catch (error) {
+      console.error("Erro ao buscar o e-mail:", error);
+    }
   };
 
   return (
-      <>
+    <>
       <Header />
-      <Container >
-
+      <Container>
         <TotalTitleArea>
-       <AreaTitle> 
-      <Title>Login</Title>
-      </AreaTitle>
-        <AreaSubtitle>
-      <Subtitle>Faça o seu cadastro conosco no link de <CadastroLink to="/cadastro" >
-      Cadastro</CadastroLink>
-      </Subtitle>
-      </AreaSubtitle>
-      </TotalTitleArea>
+          <AreaTitle>
+            <Title>Login</Title>
+          </AreaTitle>
+          <AreaSubtitle>
+            <Subtitle>
+              Faça o seu cadastro conosco no link de{" "}
+              <CadastroLink to="/cadastro">Cadastro</CadastroLink>
+            </Subtitle>
+          </AreaSubtitle>
+        </TotalTitleArea>
 
+        <AreaForm>
+          <Form onSubmit={handleSubmit}>
+            <InputContainer>
+              <Label htmlFor="email">E-mail:</Label>
+              <MailIcone src={Mail} alt="ícone de email" />
+              <Input
+                type="email"
+                id="email"
+                placeholder="Digite seu E-mail"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)} // Atualiza o estado do e-mail
+              />
+            </InputContainer>
 
-      <AreaForm>
-      <Form onSubmit={handleLogin}>
-        <InputContainer>
-          <Label htmlFor="email">E-mail:</Label>
-          <MailIcone src={Mail} alt="ícone de email" />
-          <Input
-            type='email'
-            id="email"
-            placeholder='Digite seu E-mail'
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </InputContainer>
+            <InputContainer>
+              <Label htmlFor="senha">Senha:</Label>
+              <ChaveIcone src={Chave} alt="ícone de senha" />
+              <Input
+                type={senhaVisivel ? "text" : "password"}
+                id="senha"
+                placeholder="Digite sua Senha"
+                value={senha}
+                onChange={(event) => setSenha(event.target.value)} // Atualiza o estado da senha
+              />
+            </InputContainer>
 
-        <InputContainer>
-          <Label htmlFor="senha">Senha:</Label>
-          <ChaveIcone src={Chave} alt="ícone de senha" />
-          <Input
-            type={senhaVisivel ? 'text' : 'password'}
-            id="senha"
-            placeholder='Digite sua Senha'
-            value={senha}
-            onChange={(event) => setSenha(event.target.value)}
-          />
-          </InputContainer>
+            {/* {erroEntrada && <ErrorMessage>{erroEntrada}</ErrorMessage>} */}
 
-        {erroEntrada && <ErrorMessage>{erroEntrada}</ErrorMessage>}
-
-
-        <Button type="submit" >Login</Button>
-        </Form>
-      </AreaForm>
+            <Button type="submit">Login</Button>
+          </Form>
+        </AreaForm>
       </Container>
 
       <Footer />
-    
     </>
   );
 }
